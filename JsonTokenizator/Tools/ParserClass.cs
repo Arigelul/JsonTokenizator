598b96ab721480906a402d0816b57991e9fbaac2
Igor Koryakin <igor.koryakin1203@outlook.com>
2021-12-02 11:10:13 +0200

    Add GetRawTokenIndex method in ParserClass.cs

--- JsonTokenizator/Tools/ParserClass.cs
+++ JsonTokenizator/Tools/ParserClass.cs
@@ -17,9 +17,10 @@ namespace JsonTokenizator.Tools
                 switch (json[i])
                 {
                     case '{':
-                        
+                        var jObject = new JObject() { Type = JTokenType.Object };
                         break;
                     case '[':
+                        var jArray = new JArray() { Type = JTokenType.Array };
                         break;
                     case ',':
                         break;
@@ -28,7 +29,7 @@ namespace JsonTokenizator.Tools
             return new JObject();
         }
 
-        static string ReadJson(string path = @"C:\Users\Igor\Desktop\Example2min.txt")
+        internal string ReadJson(string path = @"C:\Users\Igor\Desktop\Example2min.txt")
         {
             return File.ReadAllText(path);
         }
@@ -39,13 +40,23 @@ namespace JsonTokenizator.Tools
         //    return m.Success ? m.Index : -1;
         //}
 
-        public static int GetRawToken (string str, char delim_open, char delim_close)
+        internal int GetRawTokenIndex(string source_str, char delim_open, char delim_close)
         {
-            int index = str.IndexOf(delim_close);
-            if (str.Substring(0, index).Count(c => c.Equals(delim_open)) == str.Substring(0, index).Count(c => c.Equals(delim_close)))
-                return index;
-            else if (index != str.Length - 1) str = str.Substring(index + 1);
-            return 0;
+            var str = source_str;
+            int index = 0;
+            while (str.Length > 0)
+            {
+                int localIndex = str.IndexOf(delim_close) + 1;
+                if (localIndex != -1)
+                {
+                    index += localIndex;
+                    if (source_str.Substring(0, index).Count(c => c.Equals(delim_open)) == source_str.Substring(0, index).Count(c => c.Equals(delim_close)))
+                        return index;
+                    str = str.Substring(localIndex);
+                }
+                else break;
+            }
+            return -1;
         }
     }
 }
